2ab97e2efe326327726cbb5ff9d67c3969ad84e7
bossmoon229 <alinaignatich@gmail.com>
2025-01-28 19:45:40 +0200

    recreate function into class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header/>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
