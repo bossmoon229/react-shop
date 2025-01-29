4782e35eb86d4865751a840f80cfd11571a0d448
bossmoon229 <alinaignatich@gmail.com>
2025-01-29 20:31:38 +0200

    create shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaBasketShopping } from "react-icons/fa6";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
-        <div>
-            <span className='logo'>House Staff</span>
-            <ul className='nav'>
-                <li>About us</li>
-                <li>Contacts</li>
-                <li>Profile</li>
-            </ul>
-        </div>
-        <div className='presentation'></div>
+      <div>
+        <span className="logo">House Staff</span>
+        <ul className="nav">
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>Profile</li>
+        </ul>
+        <FaBasketShopping
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop-cart-btn ${cartOpen && 'active'}`}
+        />
+
+        {cartOpen && (
+          <div className="shop-cart"> 
+
+          </div>
+        )}
+      </div>
+      <div className="presentation"></div>
     </header>
-  )
+  );
 }
